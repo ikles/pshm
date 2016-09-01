2d1ea4fe25b463e601ef6611e8f9b6cc1c1f5c17
Igor Kleshev <membship@ya.ru>
2016-09-01 11:05:59 +0700

    start

---
+++ app/js/common.js
@@ -0,0 +1,25 @@
+$(function() {
+
+	$(".toggle-mnu").click(function() {
+		$(this).toggleClass("on");
+		$(".main-mnu").slideToggle();
+		return false;
+	});
+
+
+	$(".left_form form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+			$(".left_form form").trigger("reset");
+		});
+		return false;
+	});
+
+
+
+}); //ready
